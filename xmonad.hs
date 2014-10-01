55445a090af874ff2f48d70c9fa43bfff0a71713
dearieme <scott@dearie.me.uk>
2014-10-01 15:32:06 +0100

    Xmonad and tmux layout tweaks

--- xmonad.hs
+++ xmonad.hs
@@ -3,7 +3,8 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig
 import XMonad.Util.SpawnOnce
 
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+main :: IO ()
+main = xmonad =<< statusBar "xmobar" myPP toggleStrutsKey myConfig
 
 -- Custom PP determines what is being written to xmobar
 myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
@@ -12,24 +13,16 @@ myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 myConfig = defaultConfig
-    { terminal    = myTerminal
-    , modMask     = myModMask
-    , borderWidth = myBorderWidth
-    , startupHook = myStartupHook
+    { terminal    = "urxvtc"
+    , modMask     = mod4Mask
+    , borderWidth = 1
+    , startupHook = spawnOnce "/home/scott/bin/tmux_ide.sh"
     }
     `additionalKeysP`
-    [ ("M-<F12>",  spawn "xscreensaver-command -lock")
+    [ ("M-<F12>",                 spawn "xscreensaver-command -lock")
     , ("<XF86MonBrightnessUp>",   spawn "xbacklight +5")
     , ("<XF86MonBrightnessDown>", spawn "xbacklight -5")
     -- For post kb hotplug...
     , ("M-<F11>", spawn "setxkbmap gb && xmodmap /home/scott/.Xmodmap")
     ]
 
-myTerminal    = "urxvtc"
-myModMask     = mod4Mask
-myBorderWidth = 1
-myBar         = "xmobar"
-
-myStartupHook :: X()
-myStartupHook =
-  spawnOnce "/home/scott/bin/tmux_ide.sh"
